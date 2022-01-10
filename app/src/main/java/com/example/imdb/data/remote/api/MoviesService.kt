62e4f780f107b649d020b1b7fb89402e31c64490
erennemli <59278933+erennemli@users.noreply.github.com>
2022-01-10 14:11:47 +0300

    change db to movie db instead of the old one

--- app/src/main/java/com/example/imdb/data/remote/api/MoviesService.kt
+++ app/src/main/java/com/example/imdb/data/remote/api/MoviesService.kt
@@ -3,13 +3,18 @@ package com.example.imdb.data.remote.api
 import com.example.imdb.BuildConfig
 import com.example.imdb.data.remote.model.movies.ongoing.GetOngoingMoviesResponseModel
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MoviesService {
 
-    @GET(IN_THEATERS)
-    suspend fun getOngoingMovies(): GetOngoingMoviesResponseModel?
+    @GET(ONGOING_MOVIES)
+    suspend fun getOngoingMovies(
+        @Query(API_KEY_STRING) apiKey: String? = BuildConfig.API_KEY
+    ): GetOngoingMoviesResponseModel?
 
     companion object {
-        private const val IN_THEATERS = "InTheaters/" + BuildConfig.API_KEY
+        private const val API_KEY_STRING = "api_key"
+        private const val ONGOING_MOVIES = "movie/now_playing"
     }
 }
